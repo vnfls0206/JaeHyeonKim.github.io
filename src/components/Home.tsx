036502d62dd4d5c3494b717249aa63daea2440a2
jh <vnfls0206@gmail.com>
2023-08-21 19:01:19 +0900

    first commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export interface IHomeProps {
+  //children?: React.ReactNode,
+}
+
+const Home = (
+  {}: IHomeProps
+) => {
+
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import Home from 'components/Home';
+
+const IndexPage = function () {
+  return <Home/>;
+};
+
+export default IndexPage;
